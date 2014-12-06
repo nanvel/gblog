c09b6105f30a156cfe60ba39496f145d5d61b0b1
Oleksandr Polyeno <polyenoom@gmail.com>
2014-12-06 23:49:19 +0200

    fixed path to post on github

--- update.py
+++ update.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
                     date=arrow.get(created).strftime('%B %d, %Y'),
                     date_link='#a={timestamp}'.format(timestamp=created - (created % 86400)),
                     share_link='#b={timestamp}&l=1'.format(timestamp=created),
-                    github_link='{git_url}/tree/master/content/{path}'.format(
+                    github_link='{git_url}/tree/master/{path}'.format(
                         git_url=options.options.git_url,
                         path='/'.join(path.split('/')[-3:])),
                     timestamp=created)
